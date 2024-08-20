82a6f3a5b314c628eafd6f25e31700bf8e072056
Joilson Rodrigues <62257507+joilson-dev@users.noreply.github.com>
2024-08-19 23:30:48 -0300

    global-styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header"
+import GlobalStyle from "./global-styles"
 import AppRoutes from "./router/AppRoutes"
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Header />
       <AppRoutes />
     </>
