a1fd7ad1a655fa2e720350029346f5ad31568bba
Joilson Rodrigues <62257507+joilson-dev@users.noreply.github.com>
2024-08-24 17:59:03 -0300

    making movie listing

---
+++ src/components/Title-styles.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const TitleStyles = styled.h1`
+color: #FFFFFF;
+height: 78px;
+width: 100%;
+display: flex;
+align-items: center;
+justify-content: center;
+
+font-size: 24px;
+font-weight: 400;
+line-height: 39.13px;
+letter-spacing: 0.04em;
+
+`;
--- src/pages/movies/Movies.jsx
+++ src/pages/movies/Movies.jsx
@@ -1,8 +1,35 @@
+import { useEffect, useState } from "react";
+import { TitleStyles } from "../../components/Title-styles";
+import axios from "axios";
+
+
 function Movies() {
+    const [films, setFilms] = useState(null);
+
+    useEffect(() => {
+        const getFilms = axios.get("https://mock-api.driven.com.br/api/v8/cineflex/movies")
+        getFilms.then(req => { setFilms(req.data) })
+    }, [])
+
+    console.log(`Films =>>>>> ${films}`)
+
     return (
         <>
-            <p>Filmes</p>
+            <TitleStyles>Em cartaz</TitleStyles>
+
+            {films ? (
+                <ul>
+                    {films.map((film) => (
+                        <li key={film.id}>
+                            <img src={film.posterURL} alt={film.title} />
+                        </li>
+                    ))}
+                </ul>
+            ) : null}
+
         </>
+
+
     )
 }
 
